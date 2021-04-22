488d3bb9b8bfa3e0f804878f26d981663b004c4a
Dani Belz <dani.belz@gmail.com>
2021-04-22 23:31:36 +0200

    Refactored edge detection from OpenCV to own code

--- workspace.py
+++ workspace.py
@@ -5,7 +5,6 @@ from tkinter import ttk, Canvas, NW
 import os
 from configspace import Configspace
 import time
-import cv2
 
 # https://stackoverflow.com/questions/18943387/how-to-analyse-bitmap-image-in-python-using-pil
 # http://www.pythonclub.org/_media/modules/pil/pil.pdf
@@ -28,21 +27,7 @@ class Workspace:
         self.robotArray = np.array(self.robotImage)
         self.robotPhoto = ImageTk.PhotoImage(self.robotImage)
 
-        # For the improved version load the robot bitmap using OpenCV and
-        # let it find the contours of the robot.
-        # (This code is from the official OpenCV tutorial)
-        robotCVImg = cv2.imread(robotImagePath)
-        gray = cv2.cvtColor(robotCVImg, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-        # Store the pixels of the contour in tuples which saves us some runtime
-        # below when iterating over the list on each collission detection.
-        # Contour 0 is the frame of the robot bitmap, contour 1 is the robot "circle"
-        # and contour 2 is the small dot on the robot.
-        self.robotContourPixels = []
-        for c in contours[1]:
-            self.robotContourPixels.append((int(c[0][0]), int(c[0][1])))
+        self.__findEdges()
 
         self.label = ttk.Label(root, image = self.envPhoto)
 
@@ -62,6 +47,45 @@ class Workspace:
         self.label.pack(side = "bottom", fill = "both", expand = "yes")
 
     # -------------------------------------------------------------------------
+    def __findEdges(self):
+        
+        self.robotEdgePixels = []
+        
+        # This is pretty straight forward for our simple case at hand.
+        # A black pixel with both black and white neighbors is defined as an
+        # edge pixel.
+        # We keep it even simpler by just using 4 neighbors instead of 8, so
+        # in round figures (what the robot actually is) we don't get _every_
+        # pixel but those that are relevant for collision detection.
+        for x in range(self.robotImage.size[0] - 1):
+            for y in range(self.robotImage.size[1] - 1):
+                pixel = self.robotImage.getpixel((x, y))
+                if (not pixel):
+                    # Pixel of the robot is black, so we now check if this
+                    # is an edge pixel.
+                    #
+                    # n1 = neighbor top
+                    # n2 = neighbor right
+                    # n3 = neighbor bottom
+                    # n4 = neighbor left
+                    n1 = self.robotImage.getpixel((x, y-1))
+                    n2 = self.robotImage.getpixel((x+1, y))
+                    n3 = self.robotImage.getpixel((x, y+1))
+                    n4 = self.robotImage.getpixel((x-1, y))
+                    
+                    nSum = n1 + n2 + n3 + n4
+                    if (nSum > 0 and nSum < (4 * 255)):
+                        self.robotEdgePixels.append((x, y))
+
+                    # Possible optimizations:
+                    #  - Remove the edges from the robot's "eye" - how?
+                    #  - Special cases (although not relevant for the robot):
+                    #     - if pixel is black and y = 1 -> edge (top)
+                    #     - if pixel is black and y = height -> edge (bottom)
+                    #     - if pixel is black and x = 1 -> edge (left)
+                    #     - if pixel is black and x = width -> edge (right) 
+
+    # -------------------------------------------------------------------------
     def isInCollision(self,x,y):
         # x and y are the coords of the top left pixel of the robot
         print("--- isInCollision(" + str(x) + "," + str(y) + ")")
@@ -69,7 +93,7 @@ class Workspace:
         # To compare the runtime performance we run both collission detections
         # after one another, but only the result of the improved CD is used.
         self.__isInCollisionSequential(x, y)
-        rv = self.__isInCollisionContour(x, y)
+        rv = self.__isInCollisionWithEdges(x, y)
 
         return rv
 
@@ -98,19 +122,19 @@ class Workspace:
         return False
 
     # -------------------------------------------------------------------------
-    def __isInCollisionContour(self, x, y):
+    def __isInCollisionWithEdges(self, x, y):
 
         startTime = time.time()
 
         # Improved collision detection: do not iterate over every pixel of the
-        # robot bitmap but only over the contour pixels. The collision detection
-        # is the same as above (contour pixel over a black pixel (obstacle) in
+        # robot bitmap but only over the edge pixels. The collision detection
+        # is the same as above (edge pixel over a black pixel (obstacle) in
         # the environment is a collision).
-        for pixel in self.robotContourPixels:
+        for pixel in self.robotEdgePixels:
             if (not self.envImage.getpixel( (x + pixel[0], y + pixel[1]) )):
                 # Pixel of the env image is black, so we hit an obstacle here
-                print("Duration of contour check: ", str((time.time() - startTime) * 1000), "ms")
+                print("Duration of edges check: ", str((time.time() - startTime) * 1000), "ms")
                 return True
 
-        print("Duration of contour check: ", str((time.time() - startTime) * 1000), "ms")
+        print("Duration of edges check: ", str((time.time() - startTime) * 1000), "ms")
         return False
